27640a318b09f39826ccc52468aa38fc8bb296e6
Caleb Thomas <cjthomas00@comcast.net>
2023-06-08 15:18:14 -0600

    test: add validations tests for customer, subscriptions, and teas.

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  it { should belong_to :customer }
-  it { should belong_to :tea }
+  describe "relationships" do
+    it { should belong_to :customer }
+    it { should belong_to :tea }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:price) }
+    it { should validate_presence_of(:status) }
+    it { should validate_presence_of(:frequency) }
+
+    it { should define_enum_for(:status).with_values([:inactive, :active])}
+    it { should define_enum_for(:frequency).with_values([:weekly, :monthly, :quarterly, :annually])}
+  end
 end
